35ae286f60b6a03a0a12b1977f2ac7d50cb9277b
Sean Templeton <sean@templeton.io>
2022-04-26 21:16:37 -0700

    Remove browser rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,7 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 import babel from "rollup-plugin-babel";
 import pkg from "./package.json";
 
 export default [
-  {
-    input: "src/index.js", // your entry point
-    output: {
-      name: pkg.name, // package name
-      file: pkg.browser,
-      format: "umd",
-    },
-    plugins: [
-      resolve(),
-      commonjs(),
-      babel({
-        exclude: ["node_modules/**"],
-      }),
-    ],
-  },
   {
     input: "src/index.js", // your entry point
     output: [
